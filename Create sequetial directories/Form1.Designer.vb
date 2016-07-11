4421fd493237c1bf3a651612a868f92e61de967e
Brad.Nielsen <Brad.Nielsen@HPPC128>
2016-07-11 14:06:50 -0500

    Add project files.

---
+++ Create sequetial directories/Form1.Designer.vb
@@ -0,0 +1,133 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.buttonChooseDirectory = New System.Windows.Forms.Button()
+        Me.buttonCancel = New System.Windows.Forms.Button()
+        Me.textboxChosenDirectory = New System.Windows.Forms.Label()
+        Me.textboxNumber = New System.Windows.Forms.TextBox()
+        Me.buttonMakeDirectories = New System.Windows.Forms.Button()
+        Me.checkboxZeroPad = New System.Windows.Forms.CheckBox()
+        Me.labelNumberOfFolder = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FolderBrowserDialog1
+        '
+        '
+        'buttonChooseDirectory
+        '
+        Me.buttonChooseDirectory.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonChooseDirectory.Location = New System.Drawing.Point(12, 25)
+        Me.buttonChooseDirectory.Name = "buttonChooseDirectory"
+        Me.buttonChooseDirectory.Size = New System.Drawing.Size(140, 80)
+        Me.buttonChooseDirectory.TabIndex = 0
+        Me.buttonChooseDirectory.Text = "Choose Location"
+        Me.buttonChooseDirectory.UseVisualStyleBackColor = True
+        '
+        'buttonCancel
+        '
+        Me.buttonCancel.Location = New System.Drawing.Point(618, 307)
+        Me.buttonCancel.Name = "buttonCancel"
+        Me.buttonCancel.Size = New System.Drawing.Size(140, 80)
+        Me.buttonCancel.TabIndex = 1
+        Me.buttonCancel.Text = "Cancel"
+        Me.buttonCancel.UseVisualStyleBackColor = True
+        '
+        'textboxChosenDirectory
+        '
+        Me.textboxChosenDirectory.AutoSize = True
+        Me.textboxChosenDirectory.Location = New System.Drawing.Point(12, 9)
+        Me.textboxChosenDirectory.Name = "textboxChosenDirectory"
+        Me.textboxChosenDirectory.Size = New System.Drawing.Size(81, 13)
+        Me.textboxChosenDirectory.TabIndex = 4
+        Me.textboxChosenDirectory.Text = "No directory set"
+        '
+        'textboxNumber
+        '
+        Me.textboxNumber.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textboxNumber.Location = New System.Drawing.Point(12, 153)
+        Me.textboxNumber.Name = "textboxNumber"
+        Me.textboxNumber.Size = New System.Drawing.Size(210, 80)
+        Me.textboxNumber.TabIndex = 5
+        '
+        'buttonMakeDirectories
+        '
+        Me.buttonMakeDirectories.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.buttonMakeDirectories.Location = New System.Drawing.Point(12, 304)
+        Me.buttonMakeDirectories.Name = "buttonMakeDirectories"
+        Me.buttonMakeDirectories.Size = New System.Drawing.Size(140, 80)
+        Me.buttonMakeDirectories.TabIndex = 6
+        Me.buttonMakeDirectories.Text = "Make Folders"
+        Me.buttonMakeDirectories.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.buttonMakeDirectories.UseVisualStyleBackColor = True
+        '
+        'checkboxZeroPad
+        '
+        Me.checkboxZeroPad.AutoSize = True
+        Me.checkboxZeroPad.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!)
+        Me.checkboxZeroPad.Location = New System.Drawing.Point(15, 239)
+        Me.checkboxZeroPad.Name = "checkboxZeroPad"
+        Me.checkboxZeroPad.Size = New System.Drawing.Size(114, 38)
+        Me.checkboxZeroPad.TabIndex = 7
+        Me.checkboxZeroPad.Text = "Fixed Number" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Length"
+        Me.checkboxZeroPad.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.checkboxZeroPad.UseVisualStyleBackColor = True
+        '
+        'labelNumberOfFolder
+        '
+        Me.labelNumberOfFolder.AutoSize = True
+        Me.labelNumberOfFolder.Location = New System.Drawing.Point(12, 137)
+        Me.labelNumberOfFolder.Name = "labelNumberOfFolder"
+        Me.labelNumberOfFolder.Size = New System.Drawing.Size(178, 13)
+        Me.labelNumberOfFolder.TabIndex = 8
+        Me.labelNumberOfFolder.Text = "Enter the number of folders to make:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(770, 389)
+        Me.Controls.Add(Me.labelNumberOfFolder)
+        Me.Controls.Add(Me.checkboxZeroPad)
+        Me.Controls.Add(Me.buttonMakeDirectories)
+        Me.Controls.Add(Me.textboxNumber)
+        Me.Controls.Add(Me.textboxChosenDirectory)
+        Me.Controls.Add(Me.buttonCancel)
+        Me.Controls.Add(Me.buttonChooseDirectory)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents buttonChooseDirectory As Button
+    Friend WithEvents buttonCancel As Button
+    Friend WithEvents textboxChosenDirectory As Label
+    Friend WithEvents textboxNumber As TextBox
+    Friend WithEvents buttonMakeDirectories As Button
+    Friend WithEvents checkboxZeroPad As CheckBox
+    Friend WithEvents labelNumberOfFolder As Label
+End Class
---
+++ Create sequetial directories/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Dim globalfolderdirectory As String
+    Function CheckForAlphaCharacters(ByVal StringToCheck As String)
+        For i = 0 To StringToCheck.Length - 1
+            If Char.IsLetter(StringToCheck.Chars(i)) Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+    Private Sub FolderBrowserDialog1_HelpRequest(sender As Object, e As EventArgs) Handles FolderBrowserDialog1.HelpRequest
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles buttonChooseDirectory.Click
+        Dim Dialog As FolderBrowserDialog = New FolderBrowserDialog
+        Dim folderDirectory As String
+        folderDirectory = Space$(255)
+        Dialog.Description = "Choose a directory..."
+        Dialog.ShowDialog(Me)
+        folderDirectory = Dialog.SelectedPath
+        RTrim(folderDirectory)
+        textboxChosenDirectory.Text = folderDirectory
+        globalfolderdirectory = folderDirectory
+    End Sub
+    Private Sub buttonCancel_Click(sender As Object, e As EventArgs) Handles buttonCancel.Click
+        Close()
+
+    End Sub
+
+    Private Sub buttonMakeDirectories_Click(sender As Object, e As EventArgs) Handles buttonMakeDirectories.Click
+        'FOLDER CREATION SUB
+
+        'ERROR HANDLING
+        ''No directory selected
+        If globalfolderdirectory = "" Then
+            MsgBox("Please select a directory")
+            ''Nothing entered in box
+        ElseIf textboxNumber.Text = "" Then
+            MsgBox("Please input a number greater than 0")
+        Else
+
+            'Folder creation loop varable declarations
+            Dim userinput As Integer = Convert.ToInt64(textboxNumber.Text)
+            Dim foldername As String
+            Dim paddingstring As String
+            Dim counterstringlength As Integer
+            Dim counter As Integer = 1
+            Dim foldernumber As Integer
+            Dim vardiff As Integer = 0
+            'MAIN LOOP
+            Do While counter < userinput
+                foldernumber = counter
+                'Equal length = yes
+                If checkboxZeroPad.CheckState = 1 Then
+                    counterstringlength = counter.ToString.Length
+                    vardiff = userinput.ToString.Length - counterstringlength
+                    'to check if this is the last loop
+                    If foldernumber.ToString.Length < userinput.ToString.Length Then
+                        'padding length
+                        paddingstring = ""
+                        For x As Integer = 1 To vardiff
+                            paddingstring += "0"
+                        Next
+                        'Padded Concatination
+                        foldername = globalfolderdirectory.ToString + "\" + paddingstring + foldernumber.ToString
+                        My.Computer.FileSystem.CreateDirectory(foldername)
+
+                    Else
+                        'Concatination without padding
+                        foldername = globalfolderdirectory.ToString + "\" + foldernumber.ToString
+                        My.Computer.FileSystem.CreateDirectory(foldername)
+
+
+                    End If
+                Else
+                    'Equal length = no
+                    foldername = globalfolderdirectory.ToString + "\" + foldernumber.ToString
+                    My.Computer.FileSystem.CreateDirectory(foldername)
+                End If
+                counter += 1
+            Loop
+            foldername = globalfolderdirectory.ToString + "\" + userinput.ToString
+            My.Computer.FileSystem.CreateDirectory(foldername)
+            labelNumberOfFolder.Text = userinput.ToString + " Folders were created in " + globalfolderdirectory.ToString + "\"
+        End If
+        Process.Start(globalfolderdirectory.ToString)
+    End Sub
+
+    Private Sub textboxNumber_TextChanged(sender As Object, e As EventArgs) Handles textboxNumber.TextChanged
+
+        'User input validation
+        Dim Mystring As String = textboxNumber.Text
+        If CheckForAlphaCharacters(Mystring) Then
+            MsgBox("Please enter an intager greater than 0")
+            textboxNumber.Text = ""
+        End If
+    End Sub
+
+
+End Class
